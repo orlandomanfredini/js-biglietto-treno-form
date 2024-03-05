d678d5ba892c341d52bf21fdfc730697131e87bc
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-05 16:21:47 +0100

    ADD: commit milstone 1 parziale

--- js/app.js
+++ js/app.js
@@ -1,41 +1,56 @@
-// ---- CONSEGNA//
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// // va applicato uno sconto del 20% per i minorenni
-// oppure applicato uno sconto del 40% per gli over 65.
-// L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
+// ---- MILESTONE 1:
+// Modifichiamo la logica dell’esercizio di ieri in modo che i dati non arrivino da un prompt ma da un form che l’utente deve compilare.
+// Usando esclusivamente due input: un input per i km e un input per l’età, e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+//  Cliccando sul bottone recuperate i valori delle input e calcolate il prezzo nello stesso modo che avete usato ieri.
+// La risposta finale (o output) sarà anch’essa da scrivere in console.  
 
 
-// - Chiedere (con promt) all'utente km da percorrere e età.
+// Creare collegamento utilizzano document.getElementById
 
-let trip = parseFloat(prompt('Inserisci km da percorrere') /*number*/)
-
-
-let age = parseInt(prompt('Inserisci la tua età') /* number*/)
+const elementTrip = document.getElementById('distance'); //element
 
 
-// - In base ai km da percorrere
-//   - Applico tariffa di 0,21€ / km da percorrere
-
-let tariffa = trip * 0.21; //number
-let tariffaUmana = parseFloat(tariffa.toFixed(2)) //number
+const elementAge = document.getElementById('age-user');  //element
 
 
-// - In base all'età  
-//   - Calcolo possibili sconti: 
-    // - SE passeggero ha < 18 anni applico 20% di sconto
-    // - ALTRIMENTI SE ha età > 65 anni applico 40% di sconto
-    // - ALTRIMENTI stampa tariffa normale
+const elementButton = document.getElementById('create-ticket'); //element
 
-if(age < 18){
-    console.log(tariffaUmana - tariffaUmana * 0.20 )
-} else if(age > 65){
-    console.log(tariffaUmana - tariffaUmana * 0.40)
+
+const elementFinalPrice = document.getElementById('input-price'); //element
+
+
+let tariffa = elementTrip.value * 0.21; //number
+
+
+
+
+
+
+//  Cliccando sul bottone recuperate i valori delle input e calcolate il prezzo del biglietto.
+
+elementButton.addEventListener('click', function () {
     
-} else{
-    console.log(tariffaUmana)
-}
+    const age = parseInt(elementAge.value); // number
+    const km = parseFloat(elementTrip.value); // float
 
+    const tariffa = km * 0.21; // float
 
- // - Stampo prezzo finale in console   
+    
+
+    
+
+    
+    if (age < 18) {
+        
+        elementFinalPrice.value.innerHTML = tariffa
+    
+    } else if (age > 65) {
+        elementFinalPrice.value.innerHTML = tariffa
+        
+
+    } else {
+        elementFinalPrice.value.innerHTML = tariffa
+    }
+    
+});
+
